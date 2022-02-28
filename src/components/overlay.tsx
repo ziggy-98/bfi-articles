9d10d734218daa0efa99b82dea7c21818cd4cb37
Luke Zigler <luke.zigler@canterbury.gov.uk>
2022-02-28 10:08:33 +0000

    added overlay for when the page is loading

---
+++ src/components/overlay.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import { CircularProgress } from '@mui/material';
+
+interface Props {
+    isLoading: boolean
+}
+
+const Overlay = ({isLoading}: Props) => {
+    return(
+            <Backdrop open={isLoading} sx={{color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1}}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
+    )
+}
+
+export default Overlay;
